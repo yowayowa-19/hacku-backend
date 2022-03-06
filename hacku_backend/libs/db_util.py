25ab751ebeaedd62b44fb751cf957f20a2d9ef65
t4t5u0 <ymmtryk0902@gmail.com>
2022-03-06 18:58:14 +0900

    Remove: redundant code

--- hacku_backend/libs/db_util.py
+++ hacku_backend/libs/db_util.py
@@ -4,7 +4,7 @@ import psycopg2
 
 
 def connect():
-    # conn = psycopg2.connect("dbname=suppliers user=postgres password=postgres")
+    # TODO read from config
     username = "yowayowa"
     password = "pass"
     hostname = "postgres"
@@ -24,19 +24,18 @@ def connect():
     print(type(conn))
     return conn
 
+
 def connect_delay():
     conn = None
     while not conn:
         conn = connect()
     return conn
 
+
 def create_database():
 
-
-    
-    with connect_delay() as conn ,conn.cursor() as cur:
+    with connect_delay() as conn, conn.cursor() as cur:
         cur.execute(
             "CREATE TABLE IF NOT EXISTS users (name TEXT, salt TEXT, digest TEXT);"
         )
         conn.commit()
-    conn.close()
