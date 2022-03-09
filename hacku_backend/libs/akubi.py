4c123d60e642aee55722e718bc093a26639a8d9d
t4t5u0 <ymmtryk0902@gmail.com>
2022-03-10 01:16:30 +0900

    Update: たぶんakubiうごく．

--- hacku_backend/libs/akubi.py
+++ hacku_backend/libs/akubi.py
@@ -1,15 +1,15 @@
-import datetime
+from datetime import datetime, timedelta
 
 import psycopg2
 
 from .db_util import connect
-from .view import Akubi, AkubiResult, AkubiCombo
+from .view import Akubi, AkubiCombo
 
 # controller
 
 
 def akubi_c(akubi: Akubi):
-    return AkubiResult(last_yawned_at=akubi_m(akubi)).dict()
+    return akubi_m(akubi).dict()
 
 
 # model
@@ -30,20 +30,42 @@ def akubi_m(akubi: Akubi):
 
         last_yawned_at = cur.fetchone()[0]
 
-        if yawned_at - last_yawned_at < datetime.timedelta(minutes=5):
-
-            cur.execute("SELECT * FROM ongoing_combo;")
-
-            ongoing_yawn = cur.fetchall()
-
+        if yawned_at - last_yawned_at < timedelta(minutes=5):
             cur.execute(
-                """INSERT INTO ongoing_combo (user_id, yawned_at, latitude, longitude) 
-                    VALUES(%s, %s, %s, %s) 
-                    RETURNING yawned_at;""",
-                (akubi.user_id, yawned_at, akubi.latitude, akubi.longitude),
+                """DELETE * 
+                FROM ongoing_combo 
+                RETURNING user_id, yawned_at, latitude, longitude;"""
             )
+            cur.executemany(
+                """INSERT INTO akubi (user_id, yawned_at, latitude, longitude) 
+                VALUES(%s, %s, %s, %s);""",
+                (cur.fetchall()),
+            )
+        cur.execute("SELECT * FROM ongoing_combo;")
 
-            last_yawned_at = cur.fetchone()[0]
+        ongoing_yawn = cur.fetchall()
 
-            result = AkubiCombo
-            return 
+        cur.execute(
+            """INSERT INTO ongoing_combo (user_id, yawned_at, latitude, longitude) 
+                VALUES(%s, %s, %s, %s) 
+                RETURNING yawned_at;""",
+            (akubi.user_id, yawned_at, akubi.latitude, akubi.longitude),
+        )
+
+        last_yawned_at = cur.fetchone()[0]
+
+        result = AkubiCombo(
+            user_id=akubi.user_id,
+            combo_count=len(ongoing_yawn) + 1,
+            akubis=[
+                Akubi(
+                    user_id=item[0],
+                    yawned_at=item[1],
+                    latitude=item[2],
+                    longitude=item[3],
+                )
+                for item in ongoing_yawn
+            ],
+            last_yawned_at=last_yawned_at,
+        )
+        return result
